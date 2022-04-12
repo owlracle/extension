1af41019da8cc5f8659bdd2188e5984b65ee66ee
Pablo Werlang <pswerlang@gmail.com>
2022-04-12 17:36:56 -0300

    Update create_release script

--- release/create_release.sh
+++ release/create_release.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
-cd ..
-VERSION="1.0.1"
-zip "release/owlracle-ext-v$VERSION.zip" css/*.css script/*.min.js img/* popup.html manifest.json
+VERSION="1.1.1"
+zip "release/owlracle-ext-chrome-v$VERSION.zip" css/*.css script/*.min.js img/* popup.html manifest.json
+
+mv manifest.json manifest_chrome.json
+mv manifest_edge.json manifest.json
+zip "release/owlracle-ext-edge-v$VERSION.zip" css/*.css script/*.min.js img/* popup.html manifest.json
+mv manifest.json manifest_edge.json
+mv manifest_chrome.json manifest.json

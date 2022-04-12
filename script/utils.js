581feab16bf7370f1b67386ac7a3a9fa5a8af401
werlang <pswerlang@gmail.com>
2022-04-12 02:26:18 -0300

    Add fuse network

--- script/utils.js
+++ script/utils.js
@@ -127,20 +127,23 @@ const network = {
         eth: { symbol: 'eth', name: 'Ethereum', token: 'ETH', explorer: {
             icon: 'https://etherscan.io/images/favicon3.ico', href: 'https://etherscan.io', name: 'Etherscan', apiAvailable: true,
         } },
-        ftm: { symbol: 'ftm', name: 'Fantom', token: 'FTM', explorer: {
-            icon: 'https://ftmscan.com/images/favicon.png', href: 'https://ftmscan.com', name: 'FtmScan', apiAvailable: true,
-        } },
         bsc: { symbol: 'bsc', name: 'BSC', longName: 'Binance Smart Chain', token: 'BNB', explorer: {
             icon: 'https://bscscan.com/images/favicon.ico', href: 'https://bscscan.com', name: 'BscScan', apiAvailable: true,
-        } },
-        avax: { symbol: 'avax', name: 'Avalanche', token: 'AVAX', explorer: {
-            icon: 'https://snowtrace.io/images/favicon.ico', href: 'https://snowtrace.io', name: 'SnowTrace', apiAvailable: true,
         } },
         poly: { symbol: 'poly', name: 'Polygon', token: 'MATIC', explorer: {
             icon: 'https://polygonscan.com/images/favicon.ico', href: 'https://polygonscan.com', name: 'PolygonScan', apiAvailable: true,
         } },
+        ftm: { symbol: 'ftm', name: 'Fantom', token: 'FTM', explorer: {
+            icon: 'https://ftmscan.com/images/favicon.png', href: 'https://ftmscan.com', name: 'FtmScan', apiAvailable: true,
+        } },
+        avax: { symbol: 'avax', name: 'Avalanche', token: 'AVAX', explorer: {
+            icon: 'https://snowtrace.io/images/favicon.ico', href: 'https://snowtrace.io', name: 'SnowTrace', apiAvailable: true,
+        } },
         cro: { symbol: 'cro', name: 'Cronos', token: 'CRO', explorer: {
             icon: 'https://cronoscan.com/images/favicon.ico', href: 'https://cronoscan.com/', name: 'Cronoscan', apiAvailable: true,
+        } },
+        movr: { symbol: 'movr', name: 'Moonriver', token: 'MOVR', explorer: {
+            icon: 'https://moonriver.moonscan.io/images/favicon.ico', href: 'https://moonriver.moonscan.io/', name: 'MoonScan', apiAvailable: true,
         } },
         one: { symbol: 'one', name: 'Harmony', token: 'ONE', explorer: {
             icon: 'https://explorer.harmony.one/favicon.ico', href: 'https://explorer.harmony.one', name: 'Harmony Explorer', apiAvailable: false,
@@ -151,8 +154,8 @@ const network = {
         celo: { symbol: 'celo', name: 'Celo', token: 'CELO', explorer: {
             icon: 'https://avatars.githubusercontent.com/u/37552875?s=200&v=4', href: 'https://explorer.celo.org', name: 'Celo Explorer', apiAvailable: false,
         } },
-        movr: { symbol: 'movr', name: 'Moonriver', token: 'MOVR', explorer: {
-            icon: 'https://moonriver.moonscan.io/images/favicon.ico', href: 'https://moonriver.moonscan.io/', name: 'MoonScan', apiAvailable: true,
+        fuse: { symbol: 'fuse', name: 'Fuse', token: 'FUSE', explorer: {
+            icon: 'https://explorer.fuse.io/images/favicon-543fd97558f89019d8ee94144a7e46c7.ico?vsn=d', href: 'https://explorer.fuse.io/', name: 'Fuse Explorer', apiAvailable: false,
         } },
     },
 

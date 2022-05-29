2bf2b273dfd1be1f48b88de704f0823faf064258
Pablo Werlang <pswerlang@gmail.com>
2022-05-29 04:23:48 -0300

    Create new release.

--- release/create_release.sh
+++ release/create_release.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-VERSION="1.1.1"
+VERSION="1.2"
 zip "release/owlracle-ext-chrome-v$VERSION.zip" css/*.css script/*.min.js img/* popup.html manifest.json
 
 mv manifest.json manifest_chrome.json

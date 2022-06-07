9d8f97585534b9eaabae5b2e3654a341ebac5c4c
Pablo Werlang <pswerlang@gmail.com>
2022-06-07 13:37:50 -0300

    Remove activeTab permission from manifest.
    * Remove edge manifest.

--- release/create_release.sh
+++ release/create_release.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
 VERSION="1.2.0.3"
-zip "release/owlracle-ext-chrome-v$VERSION.zip" css/*.css script/*.min.js img/* popup.html manifest.json
-
-mv manifest.json manifest_chrome.json
-mv manifest_edge.json manifest.json
-zip "release/owlracle-ext-edge-v$VERSION.zip" css/*.css script/*.min.js img/* popup.html manifest.json
-mv manifest.json manifest_edge.json
-mv manifest_chrome.json manifest.json
+zip "release/owlracle-ext-v$VERSION.zip" css/*.css script/*.min.js img/* popup.html manifest.json
